c1b2356fbdb5254e9709303bf2a51ba77cd9858c
Michał Łomnicki <michal.lomnicki@gmail.com>
2013-02-09 16:52:43 +0100

    Ignore generated files.

--- release/app-debug.js
+++
@@ -1,7 +0,0 @@
-
-document.write('<scri'+'pt src="release/toaster/utils.js"></scr'+'ipt>')
-document.write('<scri'+'pt src="release/toaster/local_storage.js"></scr'+'ipt>')
-document.write('<scri'+'pt src="release/toaster/use_case.js"></scr'+'ipt>')
-document.write('<scri'+'pt src="release/toaster/gui.js"></scr'+'ipt>')
-document.write('<scri'+'pt src="release/toaster/glue.js"></scr'+'ipt>')
-document.write('<scri'+'pt src="release/toaster/app.js"></scr'+'ipt>')
